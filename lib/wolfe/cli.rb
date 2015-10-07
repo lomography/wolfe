56132793aabfa0cadd1d596cdf1b5089b96175f9
Martin Sereinig <martin.sereinig@lomography.com>
2015-10-07 13:11:22 +0200

    command line should output error message if an argumenterror occurs

--- lib/wolfe/cli.rb
+++ lib/wolfe/cli.rb
@@ -1,12 +1,14 @@
 require 'thor'
 
 module Wolfe
-
   class CLI < Thor
     desc "cleanup path/to/file.yml", "Will clean up files according to the rules specified in given file."
     def cleanup file
-      Wolfe.run_cleanup file
+      begin
+        Wolfe.run_cleanup file
+      rescue ArgumentError => e
+        puts "Error: #{e}"
+      end
     end
   end
-
 end
